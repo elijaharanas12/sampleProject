f25fef1aecd98428a1c036d8e5bb5b12274188ec
elijaharanas12 <zeprotechnogaming12@gmail.com>
2019-12-31 18:17:48 +0800

    updates on edong file

--- edong.php
+++ edong.php
@@ -1,1 +1,21 @@
 edong
+
+cd /c/xampp/htdocs
+start .
+ls
+ls -a
+dir
+dir -a
+
+git status
+git add .
+git commit -am "first commit"
+git log
+git reflog
+git reset --hard d546gf4
+git config -l
+git config --global user.name elijaharanas12_1
+git config user.name
+git remote add origin https://github.com/elijaharanas12/sampleProject.git
+git remote get-url origin
+	// https://github.com/elijaharanas12/sampleProject.git
